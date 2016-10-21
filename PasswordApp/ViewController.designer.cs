b2d3d597e0c27c03bf22793bbd7f5af59d021ba1
ItzPerkinz <joey.perkins.ksu@gmail.com>
2016-10-20 21:50:19 -0500

    Initial Build

---
+++ PasswordApp/ViewController.designer.cs
@@ -0,0 +1,159 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace PasswordApp
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button_Pound { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button_Star { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button0 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Login_Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView Main_Menu { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField User_Entry { get; set; }
+
+        [Action ("Button1_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Button1_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Button_Pound != null) {
+                Button_Pound.Dispose ();
+                Button_Pound = null;
+            }
+
+            if (Button_Star != null) {
+                Button_Star.Dispose ();
+                Button_Star = null;
+            }
+
+            if (Button0 != null) {
+                Button0.Dispose ();
+                Button0 = null;
+            }
+
+            if (Button1 != null) {
+                Button1.Dispose ();
+                Button1 = null;
+            }
+
+            if (Button2 != null) {
+                Button2.Dispose ();
+                Button2 = null;
+            }
+
+            if (Button3 != null) {
+                Button3.Dispose ();
+                Button3 = null;
+            }
+
+            if (Button4 != null) {
+                Button4.Dispose ();
+                Button4 = null;
+            }
+
+            if (Button5 != null) {
+                Button5.Dispose ();
+                Button5 = null;
+            }
+
+            if (Button6 != null) {
+                Button6.Dispose ();
+                Button6 = null;
+            }
+
+            if (Button7 != null) {
+                Button7.Dispose ();
+                Button7 = null;
+            }
+
+            if (Button8 != null) {
+                Button8.Dispose ();
+                Button8 = null;
+            }
+
+            if (Button9 != null) {
+                Button9.Dispose ();
+                Button9 = null;
+            }
+
+            if (Login_Button != null) {
+                Login_Button.Dispose ();
+                Login_Button = null;
+            }
+
+            if (Main_Menu != null) {
+                Main_Menu.Dispose ();
+                Main_Menu = null;
+            }
+
+            if (User_Entry != null) {
+                User_Entry.Dispose ();
+                User_Entry = null;
+            }
+        }
+    }
+}
